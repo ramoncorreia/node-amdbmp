31e8cff1b0f4c5998b8527a250847ec47c7b2fbc
ramoncorreia <113454672+ramoncorreia@users.noreply.github.com>
2022-09-13 12:22:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-2avemt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-2avemt)
---
+++ index.js
@@ -0,0 +1,5 @@
+const saldoGabriel = 1000
+const saldoVanessa = 10000
+
+const total = saldoGabriel + saldoVanessa
+console.log(total)
